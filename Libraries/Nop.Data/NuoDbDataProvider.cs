2545bdbd3887f439eb36501071c4e45674de1b31
Jiri Cincura <jiri@x2develop.com>
2013-09-01 07:50:46 +0200

    nopCommerce

---
+++ Libraries/Nop.Data/NuoDbDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Nop.Data.Initializers;
+using NuoDb.Data.Client;
+using NuoDb.Data.Client.EntityFramework;
+
+namespace Nop.Data
+{
+    public class NuoDbDataProvider : BaseEfDataProvider
+    {
+        public override IDbConnectionFactory GetConnectionFactory()
+        {
+            return new NuoDbConnectionFactory();
+        }
+
+        public override void SetDatabaseInitializer()
+        {
+            Database.SetInitializer(new NuoDbDatabaseInitializer<NopObjectContext>());
+        }
+
+        public override bool StoredProceduredSupported
+        {
+            get { return false; }
+        }
+
+        public override DbParameter GetParameter()
+        {
+            return new NuoDbParameter();
+        }
+    }
+}
